696099742c2079da122935e9edd60e6c233a695d
MuradHoosen <hoosenmurad@icloud.com>
2025-05-07 15:38:00 +0200

    Update caniuse-lite version in package-lock.json and refactor user authentication in AddTransformationTypePage to await auth() and rename user variable for clarity.

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -2,16 +2,16 @@ import Header from '@/components/shared/Header'
 import TransformationForm from '@/components/shared/TransformationForm';
 import { transformationTypes } from '@/constants'
 import { getUserById } from '@/lib/actions/user.actions';
-import { auth } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 
 const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
-  const { userId } = auth();
+  const { userId } = await auth();
   const transformation = transformationTypes[type];
 
   if(!userId) redirect('/sign-in')
 
-  const user = await getUserById(userId);
+  const userData = await getUserById(userId);
 
   return (
     <>
@@ -23,9 +23,9 @@ const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps)
       <section className="mt-10">
         <TransformationForm 
           action="Add"
-          userId={user._id}
+          userId={userData._id}
           type={transformation.type as TransformationTypeKey}
-          creditBalance={user.creditBalance}
+          creditBalance={userData.creditBalance}
         />
       </section>
     </>
